32ddc3a9e65d8d5451a9160d1e43f2bec1fdbbcf
Ani Panwar <15876706+aniruddha-panwar@users.noreply.github.com>
2024-03-31 11:34:00 -0700

    html node first pass

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,14 +1,18 @@
-
 class HTMLNode:
-    def __init__(self):
-        pass
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
 
     def to_html(self):
-        pass
+        raise NotImplementedError()
 
     def props_to_html(self):
-        pass
+        props_list = []
+        if self.props:
+            props_list = [f'''{key}="{value}"''' for key, value in self.props.items()]
+        return " "+" ".join(props_list)
 
     def __repr__(self):
-        pass
-
+        return f'''{self.__class__.__name__}({self.tag}, {self.value}, {self.children}, {self.props})'''
