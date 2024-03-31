aa4b0fe74d9f153cfea93d6dcbf6f9392daa1a91
Ani Panwar <15876706+aniruddha-panwar@users.noreply.github.com>
2024-03-31 08:53:59 -0700

    Initial boiler plate for HTML node

---
+++ src/htmlnode.py
@@ -0,0 +1,14 @@
+
+class HTMLNode:
+    def __init__(self):
+        pass
+
+    def to_html(self):
+        pass
+
+    def props_to_html(self):
+        pass
+
+    def __repr__(self):
+        pass
+
