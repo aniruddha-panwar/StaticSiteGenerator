c2dd0927a5ec75652798a2d95e385c5291aa8e9f
Ani Panwar <15876706+aniruddha-panwar@users.noreply.github.com>
2024-03-31 12:44:41 -0700

    Leaf node tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,7 +2,8 @@ class HTMLNode:
     """
     class to represent HTML tags
     """
-    def __init__(self, tag = None, value = None, children = None, props = None):
+
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -23,7 +24,7 @@ class HTMLNode:
         return props_html
 
     def __repr__(self):
-        return f'''{self.__class__.__name__}({self.tag}, {self.value}, {self.children}, {self.props})'''
+        return f"""{self.__class__.__name__}({self.tag}, {self.value}, {self.children}, {self.props})"""
 
 
 class LeafNode(HTMLNode):
@@ -31,14 +32,16 @@ class LeafNode(HTMLNode):
     class to represent HTML tags that do not have children
     For e.g. a simple <p> tag with simple text, has no nested children
     """
-    def __init__(self, tag, value, props = None):
+
+    def __init__(self, tag, value, props=None):
         super().__init__(tag=tag, value=value, children=None, props=props)
 
     def to_html(self) -> str:
         """
         Method to render the leaf node as HTML string
         """
-        if self.value is None: raise ValueError("Invalid HTML: no value")
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
         if self.tag is None:
             return self.value
 
@@ -46,4 +49,3 @@ class LeafNode(HTMLNode):
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.tag}, {self.value}, {self.props})"
-
