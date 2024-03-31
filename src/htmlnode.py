93e9d2b4b4199ab3d97d2d2fecbfa641886b7320
Ani Panwar <15876706+aniruddha-panwar@users.noreply.github.com>
2024-03-31 12:38:26 -0700

    Leaf node first pass

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,7 @@
 class HTMLNode:
+    """
+    class to represent HTML tags
+    """
     def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
         self.value = value
@@ -21,3 +24,26 @@ class HTMLNode:
 
     def __repr__(self):
         return f'''{self.__class__.__name__}({self.tag}, {self.value}, {self.children}, {self.props})'''
+
+
+class LeafNode(HTMLNode):
+    """
+    class to represent HTML tags that do not have children
+    For e.g. a simple <p> tag with simple text, has no nested children
+    """
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self) -> str:
+        """
+        Method to render the leaf node as HTML string
+        """
+        if self.value is None: raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+
+        return f"""<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"""
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.tag}, {self.value}, {self.props})"
+
