c78c2c6e07d0086623dd0fe41e9ecb08806aa36f
Ani Panwar <15876706+aniruddha-panwar@users.noreply.github.com>
2024-05-26 22:06:56 -0700

    Recursive Website Gen

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 import os
 from textnode import TextNode
 from copy_static_to_public import copy_files
-from gen_page import generate_page
+from gen_page import generate_pages_recursive
+
 
 def main():
     print("Copying files from static/ to public/")
     copy_files()
 
     print("Generating HTML from Markdown")
-    generate_page(
-        from_path=os.path.join("./content","index.md"),
-        template_path=os.path.join("./","template.html"),
-        dest_path=os.path.join("./public","index.html")
-    )
+    # generate_page(
+    #     from_path=os.path.join("./content","index.md"),
+    #     template_path=os.path.join("./","template.html"),
+    #     dest_path=os.path.join("./public","index.html")
+    # )
+    generate_pages_recursive("./content", "./template.html", "./public")
+
 
 main()
